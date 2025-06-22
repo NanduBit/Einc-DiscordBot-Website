75291d15c11391a3721daa8d318862cf351d508f
Nandu <89532571+NanduBit@users.noreply.github.com>
2025-06-22 12:25:04 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,14 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Einc Discord Bot',
   description: 'Your Favorate Discord Bot',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16' },
+      { url: '/favicon-32x32.png', sizes: '32x32' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({
