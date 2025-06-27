cf511575640aaaea7fd71cb892da880c3d6d6d77
Nandu <89532571+NanduBit@users.noreply.github.com>
2025-06-27 22:35:31 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 
 
 const UserSchema = new mongoose.Schema({
-  balance: Number,
-  userId: String,
+  userId: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    balance: {
+        type: Number,
+        default: 0,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
 }, { collection: 'users' });
 
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
